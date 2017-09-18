676a0cfef352c59bb8e428d992e6d3323998e29c
evan <eg1290@gmail.com>
2017-09-18 12:55:09 -0400

    initial API

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
 var fs         = require('fs')
   , express    = require('express')
   , bodyParser = require('body-parser')
+  , cors       = require('cors')
   , port = 8080;
 
-let data = JSON.parse(fs.readFileSync("data/game-scores.json"));
+let data = parse_trees(JSON.parse(fs.readFileSync("GWDD.json")));
 
 var app = express()
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/scores', function (req, res) {
-  if (req.query.year) {
-    res.json(data.filter(i => i.last_year < req.query.year))
-  } else {
-    res.json(data);
-  }
+let query = {
+  "family": "string",
+  "genus": "string",
+  "species": "string",
+  "region": "string",
+  "climate": "string",
+  "density": "number",
+  "ref_num": "number",
+  "id": "number"
+};
+
+app.get('/trees', function (req, res) {
+  res.json(data);
+
+  // TODO FILTER
 });
 
-app.put('/score/:score', function (req, res) {
-  if (!req.body)
-    return res.sendStatus(400)
-  let [win, lose] = req.params.score.split("-");
-  let score = data.filter(i => i.pts_win == win && i.pts_lose == lose)[0];
-
-  req.body.counter && (score.counter = req.body.counter);
-  req.body.last_year && (score.last_year = req.body.last_year);
-  req.body.first_year && (score.first_year = req.body.first_year);
-
-  res.json(data);
-});
-
-app.delete('/score/:score', function (req, res) {
-  if (!req.body)
-    return res.sendStatus(400)
-  let [win, lose] = req.params.score.split("-");
-  let score = data.filter(i => i.pts_win == win && i.pts_lose == lose)[0];
-
-  data.splice(data.indexOf(score), 1);
-  res.json(data);
-})
-
-app.post('/score', function (req, res) {
-  if (!req.body) return res.sendStatus(400)
-
-  if (req.body.counter <= 0) {
-    req.body.counter = 1;
+app.post('/trees', function (req, res) {
+  if (!req.body) {
+    return res.sendStatus(400);
   }
+
+  let data = validate(req.body);
 
   data.push(req.body);
 
   res.json(data);
 })
+
+app.put('/trees/:tree', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+
+  // TODO
+});
+
+app.delete('/trees/:tree', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+
+  let id = req.params.tree;
+
+  // TODO
+  data.splice(data.indexOf(id), 1);
+  res.json(data);
+});
+
+function validate(argument) {
+  return argument;
+}
 
 app.listen(process.env.PORT || port);
